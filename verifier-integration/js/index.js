bcbafdb178cd86fc7572900fcdd61b97fa4ca374
Nick Edgar <n.a.edgar@gmail.com>
2023-03-13 11:18:36 -0400

    Verifier: Read auth request from qrValueProofRequestExample.json'
    Copied from on-chain-verification

--- verifier-integration/js/index.js
+++ verifier-integration/js/index.js
@@ -27,46 +27,13 @@ const requestMap = new Map();
 		// GetQR returns auth request
 		async function GetAuthRequest(req,res) {
 
-			// Audience is verifier id
-			const hostUrl = "<NGROK_URL>";
 			const sessionId = 1;
-			const callbackURL = "/api/callback"
-			const audience = "did:polygonid:polygon:mumbai:2qDyy1kEo2AYcP3RT4XGea7BtxsY285szg6yP9SPrs"
-
-			const uri = `${hostUrl}${callbackURL}?sessionId=${sessionId}`;
-
-			// Generate request for basic authentication
-			const request = auth.createAuthorizationRequest(
-				'test flow',
-				audience,
-				uri,
-			);
-			
-			request.id = '7f38a193-0918-4a48-9fac-36adfdb8b542';
-			request.thid = '7f38a193-0918-4a48-9fac-36adfdb8b542';
-
-			// Add request for a specific proof
-			const proofRequest = {
-				id: 1,
-				circuitId: 'credentialAtomicQuerySigV2',
-				query: {
-				  allowedIssuers: ['*'],
-				  type: 'KYCAgeCredential',
-				  context: 'https://raw.githubusercontent.com/iden3/claim-schema-vocab/main/schemas/json-ld/kyc-v3.json-ld',
-				  credentialSubject: {
-					birthday: {
-					  $lt: 20000101,
-					},
-				  },
-			  },
-			  };
-			const scope = request.body.scope ?? [];
-			request.body.scope = [...scope, proofRequest];
+			const request = require('./requests/qrValueProofRequestExample.json');
 			 
 			// Store auth request in map associated with session ID
 			requestMap.set(`${sessionId}`, request);
 
-			return res.status(200).set('Content-Type', 'application/json').send(request);
+			return res.status(200).json(request);
         }
 
         // Callback verifies the proof after sign-in callbacks
@@ -116,5 +83,8 @@ const requestMap = new Map();
 		} catch (error) {
 		return res.status(500).send(error);
 		}
-		return res.status(200).set('Content-Type', 'application/json').send("user with ID: " + authResponse.from + " Succesfully authenticated");
-		}
+		return res.status(200).json({
+			from: authResponse.from,
+			message: "user with ID: " + authResponse.from + " succesfully authenticated"
+		});
+	}
